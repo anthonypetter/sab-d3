a42b81c4865d99ada94ca22563207d099dd7e115
anthonypetter <anthonypsalazar9@gmail.com>
2021-11-03 13:38:19 -0400

    Ch 4 - Linting changes Also updating JSON location

--- 04-animations-and-transitions/3-draw-bars-with-css-transition/updating-bars.js
+++ 04-animations-and-transitions/3-draw-bars-with-css-transition/updating-bars.js
@@ -1,11 +1,11 @@
 async function drawBars() {
 
   // 1. Access data
-  const dataset = await d3.json("./../../my_weather_data.json")
+  const dataset = await d3.json("./../../nyc_weather_data.json");
 
   // 2. Create chart dimensions
 
-  const width = 500
+  const width = 500;
   let dimensions = {
     width: width,
     height: width * 0.6,
@@ -15,108 +15,108 @@ async function drawBars() {
       bottom: 50,
       left: 50,
     },
-  }
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+  };
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
   const wrapper = d3.select("#wrapper")
     .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
   const bounds = wrapper.append("g")
-      .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+    .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`);
 
   // init static elements
   bounds.append("g")
-      .attr("class", "bins")
+    .attr("class", "bins");
   bounds.append("line")
-      .attr("class", "mean")
+    .attr("class", "mean");
   bounds.append("g")
-      .attr("class", "x-axis")
-      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+    .attr("class", "x-axis")
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`)
     .append("text")
-      .attr("class", "x-axis-label")
+    .attr("class", "x-axis-label");
 
   const drawHistogram = metric => {
-    const metricAccessor = d => d[metric]
-    const yAccessor = d => d.length
+    const metricAccessor = d => d[metric];
+    const yAccessor = d => d.length;
 
     // 4. Create scales
 
     const xScale = d3.scaleLinear()
       .domain(d3.extent(dataset, metricAccessor))
       .range([0, dimensions.boundedWidth])
-      .nice()
+      .nice();
 
     const binsGenerator = d3.bin()
       .domain(xScale.domain())
       .value(metricAccessor)
-      .thresholds(12)
+      .thresholds(12);
 
-    const bins = binsGenerator(dataset)
+    const bins = binsGenerator(dataset);
 
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(bins, yAccessor)])
       .range([dimensions.boundedHeight, 0])
-      .nice()
+      .nice();
 
     // 5. Draw data
 
-    const barPadding = 1
+    const barPadding = 1;
 
     let binGroups = bounds.select(".bins")
       .selectAll(".bin")
-      .data(bins)
+      .data(bins);
 
-    binGroups.exit().remove()
+    binGroups.exit().remove();
 
     const newBinGroups = binGroups.enter().append("g")
-        .attr("class", "bin")
+      .attr("class", "bin");
 
-    newBinGroups.append("rect")
-    newBinGroups.append("text")
+    newBinGroups.append("rect");
+    newBinGroups.append("text");
 
     // update binGroups to include new points
-    binGroups = newBinGroups.merge(binGroups)
+    binGroups = newBinGroups.merge(binGroups);
 
     const barRects = binGroups.select("rect")
-        .attr("x", d => xScale(d.x0) + barPadding)
-        .attr("y", d => yScale(yAccessor(d)))
-        .attr("width", d => d3.max([
-          0,
-          xScale(d.x1) - xScale(d.x0) - barPadding
-        ]))
-        .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)))
+      .attr("x", d => xScale(d.x0) + barPadding)
+      .attr("y", d => yScale(yAccessor(d)))
+      .attr("width", d => d3.max([
+        0,
+        xScale(d.x1) - xScale(d.x0) - barPadding
+      ]))
+      .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)));
 
     const barText = binGroups.select("text")
-        .attr("x", d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-        .attr("y", d => yScale(yAccessor(d)) - 5)
-        .text(yAccessor)
+      .attr("x", d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr("y", d => yScale(yAccessor(d)) - 5)
+      .text(yAccessor);
 
-    const mean = d3.mean(dataset, metricAccessor)
+    const mean = d3.mean(dataset, metricAccessor);
 
     const meanLine = bounds.selectAll(".mean")
-        .attr("x1", xScale(mean))
-        .attr("x2", xScale(mean))
-        .attr("y1", -20)
-        .attr("y2", dimensions.boundedHeight)
+      .attr("x1", xScale(mean))
+      .attr("x2", xScale(mean))
+      .attr("y1", -20)
+      .attr("y2", dimensions.boundedHeight);
 
     // 6. Draw peripherals
 
     const xAxisGenerator = d3.axisBottom()
-      .scale(xScale)
+      .scale(xScale);
 
     const xAxis = bounds.select(".x-axis")
-      .call(xAxisGenerator)
+      .call(xAxisGenerator);
 
     const xAxisLabel = xAxis.select(".x-axis-label")
-        .attr("x", dimensions.boundedWidth / 2)
-        .attr("y", dimensions.margin.bottom - 10)
-        .text(metric)
-  }
+      .attr("x", dimensions.boundedWidth / 2)
+      .attr("y", dimensions.margin.bottom - 10)
+      .text(metric);
+  };
 
   const metrics = [
     "windSpeed",
@@ -127,18 +127,18 @@ async function drawBars() {
     "windBearing",
     "temperatureMin",
     "temperatureMax",
-  ]
-  let selectedMetricIndex = 0
-  drawHistogram(metrics[selectedMetricIndex])
+  ];
+  let selectedMetricIndex = 0;
+  drawHistogram(metrics[selectedMetricIndex]);
 
   const button = d3.select("body")
     .append("button")
-      .text("Change metric")
+    .text("Change metric");
 
-  button.node().addEventListener("click", onClick)
+  button.node().addEventListener("click", onClick);
   function onClick() {
-    selectedMetricIndex = (selectedMetricIndex + 1) % metrics.length
-    drawHistogram(metrics[selectedMetricIndex])
+    selectedMetricIndex = (selectedMetricIndex + 1) % metrics.length;
+    drawHistogram(metrics[selectedMetricIndex]);
   }
 }
-drawBars()
+drawBars();
--- 04-animations-and-transitions/4-draw-line/updating-line.js
+++ 04-animations-and-transitions/4-draw-line/updating-line.js
@@ -1,14 +1,14 @@
 async function drawLineChart() {
 
   // 1. Access data
-  let dataset = await d3.json("./../../my_weather_data.json")
+  let dataset = await d3.json("./../../nyc_weather_data.json");
 
   // 2. Create chart dimensions
 
-  const yAccessor = d => d.temperatureMax
-  const dateParser = d3.timeParse("%Y-%m-%d")
-  const xAccessor = d => dateParser(d.date)
-  dataset = dataset.sort((a,b) => xAccessor(a) - xAccessor(b)).slice(0, 100)
+  const yAccessor = d => d.temperatureMax;
+  const dateParser = d3.timeParse("%Y-%m-%d");
+  const xAccessor = d => dateParser(d.date);
+  dataset = dataset.sort((a,b) => xAccessor(a) - xAccessor(b)).slice(0, 100);
 
   let dimensions = {
     width: window.innerWidth * 0.9,
@@ -19,30 +19,30 @@ async function drawLineChart() {
       bottom: 40,
       left: 60,
     },
-  }
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+  };
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
   const wrapper = d3.select("#wrapper")
     .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
   const bounds = wrapper.append("g")
-      .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+    .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`);
 
   // init static elements
   bounds.append("rect")
-      .attr("class", "freezing")
+    .attr("class", "freezing");
   bounds.append("path")
-      .attr("class", "line")
+    .attr("class", "line");
   bounds.append("g")
-      .attr("class", "x-axis")
-      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+    .attr("class", "x-axis")
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`);
   bounds.append("g")
-      .attr("class", "y-axis")
+    .attr("class", "y-axis");
 
   const drawLine = (dataset) => {
 
@@ -50,62 +50,62 @@ async function drawLineChart() {
 
     const yScale = d3.scaleLinear()
       .domain(d3.extent(dataset, yAccessor))
-      .range([dimensions.boundedHeight, 0])
+      .range([dimensions.boundedHeight, 0]);
 
-    const freezingTemperaturePlacement = yScale(32)
+    const freezingTemperaturePlacement = yScale(32);
     const freezingTemperatures = bounds.select(".freezing")
-        .attr("x", 0)
-        .attr("width", dimensions.boundedWidth)
-        .attr("y", freezingTemperaturePlacement)
-        .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
+      .attr("x", 0)
+      .attr("width", dimensions.boundedWidth)
+      .attr("y", freezingTemperaturePlacement)
+      .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement);
 
     const xScale = d3.scaleTime()
       .domain(d3.extent(dataset, xAccessor))
-      .range([0, dimensions.boundedWidth])
+      .range([0, dimensions.boundedWidth]);
 
     // 5. Draw data
 
     const lineGenerator = d3.line()
       .x(d => xScale(xAccessor(d)))
-      .y(d => yScale(yAccessor(d)))
+      .y(d => yScale(yAccessor(d)));
 
     const line = bounds.select(".line")
-        .attr("d", lineGenerator(dataset))
+      .attr("d", lineGenerator(dataset));
 
     // 6. Draw peripherals
 
     const yAxisGenerator = d3.axisLeft()
-      .scale(yScale)
+      .scale(yScale);
 
     const yAxis = bounds.select(".y-axis")
-      .call(yAxisGenerator)
+      .call(yAxisGenerator);
 
     const xAxisGenerator = d3.axisBottom()
-      .scale(xScale)
+      .scale(xScale);
 
     const xAxis = bounds.select(".x-axis")
-      .call(xAxisGenerator)
-  }
-  drawLine(dataset)
+      .call(xAxisGenerator);
+  };
+  drawLine(dataset);
 
   // update the line every 1.5 seconds
-  setInterval(addNewDay, 1500)
+  setInterval(addNewDay, 1500);
   function addNewDay() {
     dataset = [
       ...dataset.slice(1),
       generateNewDataPoint(dataset)
-    ]
-    drawLine(dataset)
+    ];
+    drawLine(dataset);
   }
 
   function generateNewDataPoint(dataset) {
-    const lastDataPoint = dataset[dataset.length - 1]
-    const nextDay = d3.timeDay.offset(xAccessor(lastDataPoint), 1)
+    const lastDataPoint = dataset[dataset.length - 1];
+    const nextDay = d3.timeDay.offset(xAccessor(lastDataPoint), 1);
 
     return {
       date: d3.timeFormat("%Y-%m-%d")(nextDay),
       temperatureMax: yAccessor(lastDataPoint) + (Math.random() * 6 - 3),
-    }
+    };
   }
 }
-drawLineChart()
+drawLineChart();
