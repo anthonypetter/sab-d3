bea6ee5fb6d486f5152af581e1cb5d12709ea5f4
anthonypetter <anthonypsalazar9@gmail.com>
2021-11-15 10:32:47 -0500

    Ch 6 - Basic loading and organization of some data.

--- 06-making-a-map/draw-map.js
+++ 06-making-a-map/draw-map.js
@@ -1,5 +1,21 @@
 async function drawMap() {
-  // your code goes here
+  const countryShapes = await d3.json("../resources/world-geojson.json");
+  const dataset = await d3.csv("../resources/data_bank_data.csv");
+
+  const countryNameAccessor = d => d.properties["NAME"];
+  const countryIdAccessor = d => d.properties["ADM0_A3_IS"];
+  const metric = "Population growth (annual %)";
+
+  let metricDataByCountry = {};
+  dataset.forEach(d => {
+    if (d["Series Name"] != metric) {
+      return;
+    }
+    // Prepending + to a value is a shorthand way to cast a string into a number.
+    metricDataByCountry[d["Country Code"]] = +d["2020 [YR2020]"] || 0;
+  });
+
+  console.log(metricDataByCountry);
 
 }
 drawMap();
