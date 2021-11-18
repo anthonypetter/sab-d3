492a17c41239172d3d3f33ee225fd59d2bf71ecf
anthonypetter <anthonypsalazar9@gmail.com>
2021-11-18 13:21:14 -0500

    Ch 7 - Colored season rects added.

--- 07-data-visualization-basics/chart.js
+++ 07-data-visualization-basics/chart.js
@@ -52,6 +52,62 @@ async function drawLineChart() {
 
   // 5. Draw data
 
+  // Make a seasons background color guide.
+  const seasonBoundaries = ["3-20", "6-21", "9-21", "12-21"];
+  const seasonNames = ["Spring", "Summer", "Autumn", "Winter"];
+
+  let seasonsData = [];
+  const startDate = xAccessor(dataset[0]);
+  const endDate = xAccessor(dataset[dataset.length - 1]);
+
+  /**
+   * Using .offset() forces getting two years ago. No matter the timeframe, -13
+   * months will get you at least two years in the past. I believe that the
+   * function needs to have the ENTIRE year offset to provide it. So, if we're
+   * using data that starts Jan 1 2018 we need to go back to Dec 1 2016 (13
+   * months earlier) to get 2017 to the end of 2018. We need 2017 because of the
+   * way our loop works is that we need the dimension of the first season on the
+   * graph: the start of winter on 2017-12-21.
+   */
+  const years = d3.timeYears(d3.timeMonth.offset(startDate, -13), endDate);
+  console.log(years);
+  console.log(d3.timeMonth.offset(startDate, -13));
+  years.forEach((yearDate) => {
+    const year = +d3.timeFormat("%Y")(yearDate);  // Force the date number.
+    seasonBoundaries.forEach((boundary, index) => {
+      const seasonStart = dateParser(`${year}-${boundary}`);
+      const seasonEnd = seasonBoundaries[index + 1] ? // If we're not at the end.
+        dateParser(`${year}-${seasonBoundaries[index + 1]}`) :
+        dateParser(`${year + 1}-${seasonBoundaries[0]}`); // Next year's spring.
+
+      const boundaryStart = d3.max([startDate, seasonStart]); // Left hugging.
+      const boundaryEnd = d3.min([endDate, seasonEnd]); // Right hugging.
+
+      // Get the days from dataset that lie between boundaries (ending inclusive).
+      const days = dataset.filter(d =>
+        xAccessor(d) > boundaryStart && xAccessor(d) <= boundaryEnd);
+      if (!days.length) return; // No days selected.
+
+      seasonsData.push({
+        start: boundaryStart,
+        end: boundaryEnd,
+        name: seasonNames[index],
+        mean: d3.mean(days, yAccessor), // Mean of season.
+      });
+    });
+  });
+
+  const seasonOffset = 10;
+  const seasons = bounds.selectAll(".season")
+      .data(seasonsData)
+    .enter().append("rect")
+      .attr("x", d => xScale(d.start))
+      .attr("width", d => xScale(d.end) - xScale(d.start))
+      .attr("y", seasonOffset)
+      .attr("height", dimensions.boundedHeight - seasonOffset)
+      .attr("class", d => `season ${d.name}`);
+
+
   const dots = bounds.selectAll(".dot")
     .data(dataset)
     .enter().append("circle")
