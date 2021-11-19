62b8968c4d9a1518fd5850bd7945c09a2ce20eb6
anthonypetter <anthonypsalazar9@gmail.com>
2021-11-19 17:33:18 -0500

    Ch 8 - Radar chart: Adding comments to explain how it works. Radians!

--- 08-common-charts/radar/chart.js
+++ 08-common-charts/radar/chart.js
@@ -1,5 +1,7 @@
 async function drawBars() {
 
+  const GRID_TICKS = 5;
+
   // 1. Access data
 
   let dataset = await d3.json("../../resources/nyc_weather_data.json");
@@ -29,6 +31,9 @@ async function drawBars() {
       bottom: 80,
       left: 80,
     },
+    boundedWidth: 0,
+    boundedHeight: 0,
+    boundedRadius: 0,
   };
   dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
   dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
@@ -58,20 +63,57 @@ async function drawBars() {
 
   const axis = bounds.append("g");
 
-  const gridCircles = d3.range(4).map((d, i) => (
+  const gridCircles = d3.range(GRID_TICKS + 1).map((_, i) => (
     axis.append("circle")
       .attr("cx", dimensions.boundedRadius)
       .attr("cy", dimensions.boundedRadius)
-      .attr("r", dimensions.boundedRadius * (i / 3))
+      .attr("r", dimensions.boundedRadius * (i / (GRID_TICKS)))
       .attr("class", "grid-line")
   ));
 
-  const gridLines = metrics.map((metric, i) => {
+  const gridLines = metrics.map((_metric, i) => {
+    /**
+     * You should take a quick refresher of what a radian IS by looking at the
+     * illustration gif here: https://en.wikipedia.org/wiki/Radian
+     * A radian is the radius of your circle, its length, marked on the
+     * circumference of the circle. You need 2π worth of your radius to make a
+     * full trip around the circumference.
+     * We slice our circle into six segments when we do six different values on
+     * the radar chart.
+     * So, taking 2π / 6 we're able to split our angle into six slices around
+     * a full circle. Multiply by i to get how much of an angle we want.
+     * Subtracting 0.5π rotates the slices by 90° CCW (you can tell which way
+     * it goes by inspecting the 1st, 2nd, ... lines that get drawn). So that
+     * instead of the first line being to the right (as is standard form for
+     * representing the start of a circle) it points straight up and the
+     * remaining line slices rotate around the central point.
+     * The first line has a radian of -1.57 - which is negative half pi.
+     *      With -0.5π          Without -0.5π
+     * -1.5707963267948966    0
+     * -0.5235987755982989    1.0471975511965976
+     * 0.5235987755982987     2.0943951023931953
+     * 1.5707963267948966     3.141592653589793
+     * 2.617993877991494      4.1887902047863905
+     * 3.6651914291880914     5.235987755982988
+     * It works out because i goes from 0 to 5. Meaning we don't reach 2π
+     * (6.2831853072).
+     */
     const angle = i * ((Math.PI * 2) / metrics.length) - Math.PI * 0.5;
+
+    console.log("i * ((   Math.PI * 2   ) / metrics.length) -   Math.PI * 0.5    =       angle" +
+      `\n${i} * (<${Math.PI * 2}) /      ${metrics.length}        ) - ${Math.PI * 0.5} = ${angle}`);
+
     return axis.append("line")
-      .attr("x1", dimensions.boundedWidth / 2)
+      .attr("x1", dimensions.boundedWidth / 2)  // Middle of chart.
+      .attr("y1", dimensions.boundedHeight / 2) // Middle of chart.
+      /**
+       * We're multiplying radius by the angle (a radian value). The radius
+       * is our hypotenuse. Given we have the hypotenuse and the angle, we can
+       * find the Adjacent or the Opposite.
+       * Cosine gets us the Adjacent (x).    Sine gets us the Opposite (y).
+       *  COH Cosine -> Opposite/Hypotenuse!   SAH Sine -> Adjacent/Hypotenuse!
+       */
       .attr("x2", Math.cos(angle) * dimensions.boundedRadius + dimensions.boundedWidth / 2)
-      .attr("y1", dimensions.boundedHeight / 2)
       .attr("y2", Math.sin(angle) * dimensions.boundedRadius + dimensions.boundedWidth / 2)
       .attr("class", "grid-line");
   });
@@ -87,8 +129,7 @@ async function drawBars() {
       .style("text-anchor",
         i == 0 || i == metrics.length / 2 ? "middle" :
           i < metrics.length / 2            ? "start"  :
-            "end"
-
+            "end",
       )
       .text(metric);
   });
@@ -99,15 +140,27 @@ async function drawBars() {
       .attr("class", "line");
 
   const drawLine = (day) => {
+    /**
+     * This is a cool thing. Here lines are being drawn with d3.lineRadial().
+     * Points on the line are being placed using angles are being derived from
+     * the index of the metric being read and using its scale to figure out how
+     * long the radius should be. Finally, the line is being given a curve that
+     * gives it a treatment that closes it (that is, it connects the last point
+     * to the first).
+     * If you're wondering how the interior is filled out, well, take a look at
+     * its CSS rule. fill and fill-opacity are set in styles.css.
+     */
     const lineGenerator = d3.lineRadial()
-        .angle((metric, i) => i * ((Math.PI * 2) / metrics.length))
+        .angle((_metric, i) => i * ((Math.PI * 2) / metrics.length))
         .radius((metric, i) => metricScales[i](+day[metric] || 0))
         .curve(d3.curveLinearClosed);
 
     const line = bounds.select(".line")
         .datum(metrics)
-        .attr("d", lineGenerator)
-        .style("transform", `translate(${dimensions.boundedRadius}px, ${dimensions.boundedRadius}px)`);
+        .attr("d", lineGenerator) // lineGenerator is passed datum here.
+        .style("transform", // Place the line in the center of the chart.
+          `translate(${dimensions.boundedRadius}px, ${dimensions.boundedRadius}px)`,
+        );
   };
 
   let activeDayIndex = 0;
@@ -121,7 +174,7 @@ async function drawBars() {
 
   updateChart();
 
-  d3.select("#show-next-day").on("click", e => {
+  d3.select("#show-next-day").on("click", () => {
     activeDayIndex = (activeDayIndex + 1) % dataset.length;
     updateChart();
   });
