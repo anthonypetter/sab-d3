137d64f7a9b1e928fe7340ba7e11f56c1c4d75f5
anthonypetter <anthonypsalazar9@gmail.com>
2021-12-07 13:20:30 -0500

    Ch 10 - Add coloring to the dates.

--- 10-marginal-histogram/scatter.js
+++ 10-marginal-histogram/scatter.js
@@ -3,16 +3,21 @@ async function drawScatter() {
   // 1. Access data
 
   const dataset = await d3.json("../../resources/nyc_weather_data.json");
+  console.table(dataset[0]);
 
   // set data constants
   const xAccessor = d => d.temperatureMin;
   const yAccessor = d => d.temperatureMax;
 
+  const colorScaleYear = 2000;  // Force to be the same year.
+  const parseDate = d3.timeParse("%Y-%m-%d");
+  const colorAccessor = d => parseDate(d.date).setYear(colorScaleYear);
+
   // 2. Create chart dimensions
 
   const width = d3.min([
-    window.innerWidth * 0.75,
-    window.innerHeight * 0.75,
+    window.innerWidth * 0.85,
+    window.innerHeight * 0.85,
   ]);
   const dimensions = {
     width: width,
@@ -26,8 +31,10 @@ async function drawScatter() {
     boundedWidth: 0,
     boundedHeight: 0,
   };
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+  dimensions.boundedWidth = dimensions.width -
+    dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight = dimensions.height -
+    dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
@@ -37,7 +44,10 @@ async function drawScatter() {
       .attr("height", dimensions.height);
 
   const bounds = wrapper.append("g")
-    .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`);
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`,
+    );
 
   const boundsBackground = bounds.append("rect")
       .attr("class", "bounds-background")
@@ -69,6 +79,43 @@ async function drawScatter() {
     .range([dimensions.boundedHeight, 0])
     .nice();
 
+  // Default rainbow style.
+  // const colorScale = d3.scaleSequential()
+  //   .domain([
+  //     parseDate("2000-01-01"),
+  //     parseDate("2000-12-31"),
+  //   ])
+  //   .interpolator(d => d3.interpolateRainbow(-d));  // Inverted rainbow.
+
+  /**
+   * My own season-coloring style.
+   * It's seriously limited because it must loop with the first and last day
+   * of the year being the same. I didn't like putting the peaks of the seasons
+   * on the solstices and equinoxes, so I vaguely put them half way through.
+   * Seems about right, huh? Feb 15 being the dead of winter, April 30 being
+   * the heart of spring, August 1 being the peak of summer, and November 11
+   * being the depth of fall.
+   * I then used an online tool to get me the middle point in the gradient
+   * between Fall and Winter and named it "New Years".
+   */
+  const colorScale = d3.scaleLinear()
+    .domain([
+      parseDate(`${colorScaleYear}-01-01`),  // New Years
+      parseDate(`${colorScaleYear}-02-15`),  // Winter
+      parseDate(`${colorScaleYear}-04-30`),  // Spring
+      parseDate(`${colorScaleYear}-08-01`),  // Summer
+      parseDate(`${colorScaleYear}-11-02`),  // Fall
+      parseDate(`${colorScaleYear}-12-31`),  // New Years
+    ])
+    .range([
+      "#a47146",  // New Years
+      "#483d8b",  // Winter
+      "#00ff7f",  // Spring
+      "#ff0",     // Summer
+      "#ffa500",  // Fall
+      "#a47146",  // New Years
+    ]);
+
   // 5. Draw data
 
   const dotsGroup = bounds.append("g");
@@ -78,7 +125,10 @@ async function drawScatter() {
       .attr("class", "dot")
       .attr("cx", d => xScale(xAccessor(d)))
       .attr("cy", d => yScale(yAccessor(d)))
-      .attr("r", 4);
+      .attr("r", 4)
+      .style("fill", d => colorScale(colorAccessor(d)))
+      .style("stroke", "black")
+      .style("stroke-opacity", "0.2");
 
   // 6. Draw peripherals
 
