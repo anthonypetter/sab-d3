91c48ca8a58104019e3bce5397d39a8b4fd1d1a0
anthonypetter <anthonypsalazar9@gmail.com>
2021-12-09 16:53:52 -0500

    Ch 12 - Adding accessors, building a probability stack, and making a person generator.

--- 12-animated-sankey/chart.js
+++ 12-animated-sankey/chart.js
@@ -4,6 +4,78 @@ async function drawChart() {
 
   const dataset = await d3.json("./../resources/education.json");
 
+  // Sex.
+  const sexAccessor = d => d.sex;
+  const sexes = ["female", "male"];
+  const sexIds = d3.range(sexes.length);  // [0, 1]
+
+  // Education.
+  const educationAccessor = d => d.education;
+  const educationNames = [  // Save time just writing these ourself.
+    "<High School",
+    "High School",
+    "Some Post-secondary",
+    "Post-secondary",
+    "Associate's",
+    "Bachelor's and up",
+  ];
+  const educationIds = d3.range(educationNames.length);
+
+  // Socioeconomic Status.
+  const sesAccessor = d => d.ses;
+  const sesNames = ["low", "middle", "high"];
+  const sesIds = d3.range(sesNames.length);
+
+  /**
+   * Returns a key string.
+   * Ex: {sex: "female", ses: "middle"} gives "female--middle".
+   * @param {{sex: string, ses: string}} param0
+   * @returns
+   */
+  const getStatusKey = ({sex, ses}) => [sex, ses].join("--");
+
+  // Generating possibilities.
+  const stackedProbabilities = {};
+  dataset.forEach(startingPoint => {
+    const key = getStatusKey(startingPoint);
+    let stackedProbability = 0;
+    stackedProbabilities[key] = educationNames.map((education, i) => {
+      stackedProbability += (startingPoint[education] / 100);
+      if (i === educationNames.length - 1) {
+        return 1; // Account for rounding errors
+      } else {
+        return stackedProbability;
+      }
+    });
+  });
+  console.table(stackedProbabilities);
+
+  function generatePerson() {
+    const sex = getRandomValue(sexIds);
+    const ses = getRandomValue(sesIds);
+    const statusKey = getStatusKey({
+      sex: sexes[sex],
+      ses: sesNames[ses],
+    });
+
+    /**
+     * d3.bisect() takes an array and tells you where in the array you'd place
+     * the second argument. Since our probabilities are 0->1 it works great with
+     * Math.random(). Instead of dealing with writing a custom function that
+     * does this basic checking, it's neat to just borrow a function from d3
+     * that does it for us. Love it.
+     */
+    const probabilities = stackedProbabilities[statusKey];
+    const education = d3.bisect(probabilities, Math.random());
+
+    return { sex, ses, education };
+  }
+
+  console.log(generatePerson());
+  console.log(generatePerson());
+  console.log(generatePerson());
+
+
   // 2. Create chart dimensions
 
   const width = d3.min([
